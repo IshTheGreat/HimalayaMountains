012d4b1950c7c5af3fae996a675a459c3d325e2a
IshTheGreat <65795098+IshTheGreat@users.noreply.github.com>
2021-08-29 12:49:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function basicFacts() {
+	document.getElementById("basic-facts").innerHTML = "The Himalayas are a mountain range located in Asia, which separates India from the Tibetan Plateau."
+	document.getElementById("basic-facts-1").innerHTML = "This mountain range has some of the highest mountains peaks across the world, including the profound Mount Everest."
+	document.getElementById("basic-facts-2").innerHTML = "The 5 highest mountains in this range are:"
+	document.getElementById("basic-facts-3").innerHTML = "-Mount Everest, 29,032 feet tall."
+	document.getElementById("basic-facts-4").innerHTML = "-K2, 28,251 feet tall."
+	document.getElementById("basic-facts-5").innerHTML = "-Kangchenjunga, 28,196 feet tall."
+	document.getElementById("basic-facts-6").innerHTML = "-Nanga Parbat, 26,657 feet tall."
+	document.getElementById("basic-facts-7").innerHTML = "-Mount Kailash, 21,837 feet tall. "
+	document.getElementById("basic-facts-8").innerHTML = "These mountains cross 5 major countries of Asia:"
+	document.getElementById("basic-facts-9").innerHTML = "-Bhutan, India, China, Nepal, Pakistan"
+}
+
+function sciBehind() {
+	document.getElementById("sci-behind").innerHTML = "Some parts of the mountain range were lifted by the subduction of the Indian Tectonic Plate, causing the Himalayas borders to expand. The foundation of this range was due to the collision of the Indian and Eurasian plates. These 2 plates collided, making a convergent boundary. Convergent boundaries are when 2 plates that are moving towards each other in the same direction collide. The softer plate usually would subduct underneath the second plate, which is what the Indian Tectonic Plate had done. As a result, the sedimentary rocks in the Earth's crust were folded, which caused the Himalayas. "
+}
+
+function ecoSystem() {
+	document.getElementById("eco-system").innerHTML = "The weather of the mountains is fairly cold, but could increase up to 35 Celcius at times, and drop as far as -35 Celcius both in summer and winter. There are over 15,000 glaciers in the Himalayas! Many endangered species live in the Himalayas, such as black bears, snow leopards, and red pandas. Many different ecosystems also lie in the range, such as temperate forests, grasslands, savannas, and rich meadows. There are non-living thins as well, most commonly we find snow. There are harsh climates, and snowfalls every now and then in the winters. Most common agriculture includes rice, wheat, barley, and soybean."
+}
+
